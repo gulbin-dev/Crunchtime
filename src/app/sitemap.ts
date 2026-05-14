82d906873bcfe76e4870fa13e2aa1f8be91e5335
Joshua Glenn R. Gulbin <gulbindev@gmail.com>
2026-05-14 09:45:31 +0800

    refractor: update sitemap url (#11)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://crunchtime-demo-gulbindev.vercel.app",
+      url: "https://crunchtime-gulbin-devs-projects.vercel.app",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://crunchtime-demo-gulbindev.vercel.app/about",
+      url: "https://crunchtime-gulbin-devs-projects.vercel.app/about",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
