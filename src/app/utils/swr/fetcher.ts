8c2697b38e22672eaf108ea571d2b0382bf97221
gulbin-dev <gulbindev@gmail.com>
2026-08-27 19:05:56 +0800

    refactor: add toast notification if it fails

--- src/app/utils/swr/fetcher.ts
+++ src/app/utils/swr/fetcher.ts
@@ -1,9 +1,12 @@
+import toastStatus from "@utils/notification/toastStatus";
 export const fetcher: <T>(url: string) => Promise<T> = async (url) => {
   const res = await fetch(url);
-  if (!res.ok) {
+  if (res.status >= 500) {
     const error = new Error("An error occurred while fetching the data.");
-    // Attach extra info to the error object.
-
+    toastStatus(error.message, {
+      id: `fetch-error-${url}`,
+      status: "error",
+    });
     return error;
   }
   return res.json();
