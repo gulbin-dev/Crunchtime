8f59d15b1e62a7632b46fe9ea608d29fe606bb98
gulbin-dev <gulbindev@gmail.com>
2026-08-27 19:12:25 +0800

    refactor: ErrorPage to remove unused error logging; enhance GlobalError button text; add proxy function for account redirection

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,5 @@
 "use client"; // Error boundaries must be Client Components
 
-import { useEffect } from "react";
-
 export default function ErrorPage({
   error,
   unstable_retry,
@@ -9,13 +7,8 @@ export default function ErrorPage({
   error: Error & { digest?: string };
   unstable_retry: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
   return (
-    <div className="h-sceen">
+    <div className="flex h-screen flex-col items-center justify-center">
       <h2>Something went wrong!</h2>
       <p>{error.message}</p>
       <button
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -13,7 +13,7 @@ export default function GlobalError({
       <body>
         <h2>Something went wrong!</h2>
         <p>{error.message}</p>
-        <button onClick={() => unstable_retry()}>Try again</button>
+        <button onClick={() => unstable_retry()}>Try again!!</button>
       </body>
     </html>
   );
