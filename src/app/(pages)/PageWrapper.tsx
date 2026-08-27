1df4567ee0bdba437e5f8c71215bc24e7c5dde97
gulbin-dev <gulbindev@gmail.com>
2026-08-27 19:11:51 +0800

    feat: enhance PageWrapper with ToastContainer for notifications; update className handling; refactor Home component layout and remove unused code

--- src/app/(pages)/PageWrapper.tsx
+++ src/app/(pages)/PageWrapper.tsx
@@ -2,17 +2,28 @@
 
 import React from "react";
 import { useAppSelector } from "@hooks/redux-typed-hooks";
+import { ToastContainer } from "react-toastify";
 export default function PageWrapper({
   children,
+  className,
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   const theme = useAppSelector((state) => state.theme.theme);
   return (
     <main
       data-theme={theme}
-      className="bg-primary text-foreground-primary transition-colors duration-300"
+      className={`bg-primary text-foreground-primary relative w-full overflow-x-hidden transition-colors duration-300 ${className}`}
     >
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick={true}
+        theme="colored"
+      />
       {children}
     </main>
   );
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -4,106 +4,68 @@ import FiveTrendLoader from "@components/UI/FiveTrendLoader";
 import PageLoader from "@components/UI/PageLoader";
 import SearchUI from "@components/SearchUI";
 import PageWrapper from "./PageWrapper";
-import { popularList } from "@server/popularList";
-import { movieGenreList } from "@server/movieGenres";
-import { tvGenreList } from "@server/tvGenres";
 import CatalogSection from "./_component/CatalogSection";
 import Button from "@components/UI/Button";
-
 export default async function Home() {
-  const trending = popularList();
-  const movieGenres = movieGenreList();
-  const tvGenres = tvGenreList();
-
   return (
     <PageWrapper>
-      <div className="max-w-180 place-self-center relative w-full">
-        <div className="relative z-20">
-          <h1
-            className="text-pretty text-heading-lg font-bold pt-2 px-3
-          tablet:pt-5 tablet:ml-10  tablet:text-heading-lg tablet:text-start 
-          "
-          >
-            Discover worth to watch movies & TV shows
-          </h1>
-        </div>
-        <SearchUI />
-        <section
-          className="relative w-full h-96 overflow-hidden px-3"
-          aria-labelledby="popular-five"
+      <h1 className="text-heading-lg tablet:pt-5 tablet:ml-10 text-foreground-primary desktop:absolute tablet:text-heading-lg tablet:text-start desktop:text-white pointer-events-none relative z-20 px-3 pt-2 font-bold text-pretty">
+        Discover worth to watch movies & TV shows
+      </h1>
+      <SearchUI
+        className="desktop:hidden block"
+        inputId="search-on-small-screen"
+      />
+      <section
+        className="relative h-96 w-full overflow-hidden px-3"
+        aria-labelledby="popular-five"
+      >
+        <h2
+          className="text-foreground-light text-heading-lg tablet:top-25 tablet:left-10 pointer-events-none relative top-15 z-20 w-fit"
+          id="popular-five"
         >
-          <h2
-            className="relative w-fit z-20 top-15 text-foreground-light text-heading-lg tablet:top-25 tablet:left-10"
-            id="popular-five"
-          >
-            Top 5 Most Popular
-          </h2>
+          Top 5 Most Popular
+        </h2>
 
-          <Suspense fallback={<FiveTrendLoader />}>
-            <FiveTrend
-              trending={trending}
-              movieGenres={movieGenres}
-              tvGenres={tvGenres}
-            />
-          </Suspense>
-        </section>
-        <Suspense fallback={<PageLoader />}>
-          <CatalogSection
-            sectionTitle="Trending"
-            genre={[""]}
-            movieGenres={movieGenres}
-            tvGenres={tvGenres}
-          />
-          <CatalogSection
-            sectionTitle="Action"
-            genre={["Action"]}
-            movieGenres={movieGenres}
-            tvGenres={tvGenres}
-          />
-          <CatalogSection
-            sectionTitle="Animation"
-            genre={["Animation"]}
-            movieGenres={movieGenres}
-            tvGenres={tvGenres}
-          />
-          <CatalogSection
-            sectionTitle="Drama"
-            genre={["Drama"]}
-            movieGenres={movieGenres}
-            tvGenres={tvGenres}
-          />
+        <Suspense fallback={<FiveTrendLoader />}>
+          <FiveTrend />
         </Suspense>
-        <section className="flex flex-col" aria-labelledby="discover-more">
-          <Button className="rounded-xl p-3 text-nowrap bg-cta font-bold w-fit h-fit place-self-center mt-4">
-            DISCOVER MORE
+      </section>
+      <Suspense fallback={<PageLoader />}>
+        <CatalogSection sectionTitle="Trending" genre={[""]} />
+        <CatalogSection sectionTitle="Action" genre={["Action"]} />
+        <CatalogSection sectionTitle="Animation" genre={["Animation"]} />
+        <CatalogSection sectionTitle="Drama" genre={["Drama"]} />
+      </Suspense>
+      <section className="flex flex-col" aria-labelledby="discover-more">
+        <Button className="mt-4 h-fit w-fit place-self-center rounded-xl p-3 font-bold text-nowrap">
+          DISCOVER MORE
+        </Button>
+        <div
+          className={`tablet:flex-row tablet:justify-center tablet:gap-20 mt-5 flex flex-col gap-2 px-3 py-10`}
+        >
+          <div>
+            <h3 className="text-heading-lg">Watch Anytime, Anywhere</h3>
+            <p className="">
+              Watch on smart TVs, gaming consoles, mobile devices and computers
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-heading-lg">Watch it later</h3>
+            <p className="mt-1">
+              Download your favorite movies and TV shows offline
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-secondary my-5 flex flex-col place-self-center rounded-lg p-2">
+          <h3 className="text-heading-lg">JOIN US NOW</h3>
+          <Button className="mt-1 w-fit place-self-center px-3 py-1 font-bold">
+            SIGN UP
           </Button>
-          <div
-            className={`flex flex-col mt-5 px-3 py-10 gap-2 tablet:flex-row tablet:justify-center tablet:gap-20 `}
-          >
-            <div>
-              <h3 className="text-heading-lg">Watch Anytime, Anywhere</h3>
-              <p className="mt-1">
-                Watch on smart TVs, gaming consoles, mobile devices and
-                computers
-              </p>
-            </div>
-
-            <div>
-              <h3 className="text-heading-lg">Watch it later</h3>
-              <p className="mt-1">
-                Download your favorite movies and TV shows offline
-              </p>
-            </div>
-          </div>
-
-          <div className="flex flex-col rounded-lg place-self-center my-5 p-2 bg-secondary">
-            <h3 className=" text-heading-lg">JOIN US NOW</h3>
-            <Button className="py-1 mt-1 px-3 w-fit font-bold place-self-center">
-              SIGN UP
-            </Button>
-          </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </PageWrapper>
   );
 }
