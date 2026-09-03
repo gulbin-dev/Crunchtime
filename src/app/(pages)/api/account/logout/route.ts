30f7ab907c96291b2d27b7cd4459afd44b158360
gulbin-dev <gulbindev@gmail.com>
2026-09-03 09:12:42 +0800

    refactor: update handling cookie deletion and error logging in logout route

--- src/app/(pages)/api/account/logout/route.ts
+++ src/app/(pages)/api/account/logout/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function DELETE() {
+export async function DELETE(request: NextRequest) {
   try {
     const cookieStore = await cookies();
     const sessionId = cookieStore.get("tmdb_session_id")?.value;
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/authentication/${sessionId}`,
+      `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/authentication/session`,
       {
         method: "DELETE",
         headers: {
@@ -15,12 +15,16 @@ export async function DELETE() {
           Authorization: `Bearer ${process.env.Read_Access_Token}`,
         },
         body: JSON.stringify({ session_id: sessionId }),
+        signal: request.signal,
       },
     );
-    const data = await response.json();
-
+    const responseText = await response.text();
+    const data = responseText ? JSON.parse(responseText) : {};
+    cookieStore.delete("tmdb_session_id");
     return NextResponse.json(data);
-  } catch {
+  } catch (e) {
+    console.error("Logout Route Error:", e); // Log the actual error to your terminal!
+
     return NextResponse.json(
       { error: "It seems something went wrong." },
       { status: 500 },
