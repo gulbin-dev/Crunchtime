9b88026e817c94c481085ff2bc1cc2a2a454f09b
gulbin-dev <gulbindev@gmail.com>
2026-09-04 14:41:54 +0800

    feat: add CATALOG feature flag

--- src/app/utils/featureFlag.ts
+++ src/app/utils/featureFlag.ts
@@ -2,4 +2,5 @@ export type FeatureFlagName = keyof typeof FeatureFlags;
 
 export const FeatureFlags = {
   ACCOUNT_FLAG: process.env.NEXT_PUBLIC_FEATURE_ACCOUNT_FLAG === "true",
+  CATALOG_FLAG: process.env.NEXT_PUBLIC_FEATURE_CATALOG_FLAG === "true",
 } as const;
