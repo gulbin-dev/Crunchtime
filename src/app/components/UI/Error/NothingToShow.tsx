53501dd428f16ef350c9d20d395012a41b8589a3
gulbin-dev <gulbindev@gmail.com>
2026-09-04 14:44:13 +0800

    chore: add new error UI on Similar Cards and Recommended Cards

---
+++ src/app/components/UI/Error/NothingToShow.tsx
@@ -0,0 +1,7 @@
+export default function NothingToShow() {
+  return (
+    <div className="bg-primary-shade dark:bg-dark-shade mb-12 flex justify-center rounded-4xl p-4">
+      <span>Nothing to show</span>
+    </div>
+  );
+}
