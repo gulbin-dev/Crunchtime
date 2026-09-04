3bbe682df0d158ade6374eb5c7c3df27181ad535
gulbin-dev <gulbindev@gmail.com>
2026-09-04 14:41:32 +0800

    refactor: update desktop media query breakpoint to 1024px

--- src/app/utils/gsap.ts
+++ src/app/utils/gsap.ts
@@ -5,7 +5,7 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger, Observer);
 
 const mediaQueries = {
-  isDesktop: "(min-width: 992px)",
+  isDesktop: "(min-width: 1024px)",
   isTablet: "(min-width: 768px) and (max-width: 1023px)",
   isMobile: "(max-width: 767px)",
 };
