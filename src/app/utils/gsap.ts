51a371850fccb1cfa67aa8597a49916c2ccddb2c
gulbin-dev <gulbindev@gmail.com>
2026-09-13 05:29:05 +0800

    perf: improve performance by reducing FPS to 45 and set lag compensation

--- src/app/utils/gsap.ts
+++ src/app/utils/gsap.ts
@@ -4,6 +4,8 @@ import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger, Observer);
 
+gsap.ticker.fps(45); // set lower FPS to reduce CPU workload
+gsap.ticker.lagSmoothing(300, 96);
 const mediaQueries = {
   isDesktop: "(min-width: 1024px)",
   isTablet: "(min-width: 768px) and (max-width: 1023px)",
