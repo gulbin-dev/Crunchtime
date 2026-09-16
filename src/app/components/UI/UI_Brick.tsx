4ba53c5196ec2c6fe02ab5674efd6a599b657ecf
gulbin-dev <gulbindev@gmail.com>
2026-09-16 13:02:45 +0800

    refactor: update component's prop handling

--- src/app/components/UI/UI_Brick.tsx
+++ src/app/components/UI/UI_Brick.tsx
@@ -1,27 +1,29 @@
 "use client";
-// defining TYPE for UI_Brick parameter
-interface Prop {
-  value: string | string[];
-  ariaLabel?: string;
-  style?: string;
-}
-export default function UI_Brick({ value, ariaLabel, style }: Prop) {
+
+import { ComponentPropsWithoutRef } from "react";
+import { Genre } from "@utils/types/types";
+
+type Prop = Omit<ComponentPropsWithoutRef<"li">, "value"> & {
+  value: Genre[] | string;
+};
+
+export default function UI_Brick({ value, className, ...props }: Prop) {
   if (typeof value === "string") {
     return (
       <p
-        className={`py-0.2 aria-label bg-secondary text-foreground-dark w-fit rounded-xl px-1 ${style}`}
-        aria-label={ariaLabel || ""}
+        className={`py-0.2 aria-label bg-secondary text-foreground-dark w-fit rounded-xl px-1 ${className}`}
       >
         {value}
       </p>
     );
   }
-  return value.map((text) => (
-    <li key={text}>
+
+  return value.map((item) => (
+    <li key={item.id} {...props}>
       <p
-        className={`py-0.2 bg-secondary text-foreground-dark w-fit rounded-xl px-1 italic ${style}`}
+        className={`py-0.2 bg-secondary text-foreground-dark w-fit rounded-xl px-1 italic ${className}`}
       >
-        {text}
+        {item.name}
       </p>
     </li>
   ));
