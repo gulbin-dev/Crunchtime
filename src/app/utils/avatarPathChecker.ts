ebf21a9d254f6523fd9ed63acf16a7e02dd88e79
gulbin-dev <gulbindev@gmail.com>
2026-09-20 19:17:16 +0800

    refactor: clean code

--- src/app/utils/avatarPathChecker.ts
+++ src/app/utils/avatarPathChecker.ts
@@ -1,5 +1,5 @@
 export function avatarPathChecker(path: string | null) {
-  if (path === null) return undefined;
+  if (path === null) return;
   const isDomainPath = /^\/http/.test(path);
   if (isDomainPath) return path.slice(1);
   else return "https://image.tmdb.org/t/p/w92" + path;
